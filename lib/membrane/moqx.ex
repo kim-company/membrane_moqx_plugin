f47e4916ef92809031b7643f47639443a67dba58
Daniel Morandini <danielmorandini@me.com>
2026-07-15 13:10:55 +0200

    Scaffold project

---
+++ lib/membrane/moqx.ex
@@ -0,0 +1,18 @@
+defmodule Membrane.MOQX do
+  @moduledoc """
+  Documentation for `Membrane.MOQX`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Membrane.MOQX.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
